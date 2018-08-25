2cdb2a2c36f5a4e6b762199e568233b23005e55c
kipply <carolyfisher@gmail.com>
2018-08-24 23:21:08 -0400

    oops that was python 2

--- __main__.py
+++ __main__.py
@@ -6,7 +6,7 @@ def main():
     p = optparse.OptionParser()
     p.add_option('--person', '-p', default="world")  # sample arg handling
     options, arguments = p.parse_args()
-    print 'Hello %s' % options.person  # sample arg handling
+    print('Hello %s' % options.person)  # sample arg handling
 
 
 if __name__ == '__main__':
