976a6bed6f4353995a83aa853fb5fe04e48aa562
kipply <carolyfisher@gmail.com>
2018-08-25 10:47:15 -0400

    Update main with some test files and fixes, and update token mapper to check dictionary and work with main

--- stdlib/davidgu/polycode/functions/__main__.js
+++ stdlib/davidgu/polycode/functions/__main__.js
@@ -6,10 +6,15 @@ const lib = require("lib");
 * @param {object} map
 * @returns {any}
 */
-module.exports = (doc="", config, map, context, callback) => {
+
+const test_config = {
+  "to": "FR",
+  "from": "EN"
+}
+module.exports = (doc="testPotato banana", config, map, context, callback) => {
   console.log(context.service.identifier)
   lib[`${context.service.identifier}.get_tokens`](doc, (err, tokens) => {
-    lib[`${context.service.identifier}.token_mapper`](tokens, (err, tokens) => {
+    lib[`${context.service.identifier}.token_mapper`](test_config.to, test_config.from, tokens, JSON.parse(map), (err, tokens) => {
       lib[`${context.service.identifier}.update_doc`](doc, tokens, (err, doc) => {
         callback(null, doc);
       });
--- stdlib/davidgu/polycode/functions/token_mapper.js
+++ stdlib/davidgu/polycode/functions/token_mapper.js
@@ -1,30 +1,50 @@
 const googleTranslate = require('google-translate')(process.env.apiKey);
 const lib = require('lib');
 const Case = require('case');
-module.exports = async (tokens = ["BANANAS_APPLES_ORANGES", "camelCase", "AppleBanana", "david-case", "snakey_boi", "apple", "pineapple"], dictionary = "apple pomme\npineapple ananas", to="ES", from="EN", context) => {
-  String.prototype.replaceAll = function(find, replace) {
-    return this.split(find).join(replace);
-  }
-  const dictionaryObj = JSON.parse(`{"${dictionary.replaceAll(" ", "\":\"").replaceAll('\n', '", "')}"}`)
-  function translateText(text, fromLanguage, toLanguage) {
-    return new Promise((resolve, reject) => {
-        googleTranslate.translate(text, fromLanguage, toLanguage, (err, translation) => {
-            if (err !== null) {
-                reject(err);
-            }
-            else {
-                resolve(translation.translatedText);
-            }
-        });
-    });
-  }
+
+/**
+* @param {string} to
+* @param {string} from
+* @param {array} tokens
+* @param {object} dictionary
+* @returns {array}
+*/
+
+module.exports = async (to, from, tokens, dictionary, context) => {
   const allPromises = [];
+
+  const toLangIdx = dictionary.languages.indexOf(to);
+  const fromLangIdx = dictionary.languages.indexOf(from);
+
   for (let i = 0; i < tokens.length; i++) {
-    allPromises.push(translateText(Case.lower(tokens[i]), from, to));
+    let inDict = false;
+    dictionary.tokens.forEach(entry => { // as in dictionary entry
+      if (entry[fromLangIdx] === tokens[i].value) {
+        tokens[i].translated = entry[toLangIdx];
+        inDict = true;
+      }
+    });
+    if (!inDict) {
+      allPromises.push(translateText(Case.lower(tokens[i].value), from, to));
+    }
   }
   const results = await Promise.all(allPromises);
   results.forEach((item, i) => {
-    results[i] = Case[Case.of(tokens[i])](item);
+    tokens[i].translated = Case[Case.of(tokens[i].value)](item);
   });
-  return results;
+  console.log(results)
+  return tokens;
+
+  function translateText(text, fromLanguage, toLanguage) {
+    return new Promise((resolve, reject) => {
+      googleTranslate.translate(text, fromLanguage, toLanguage, (err, translation) => {
+        if (err !== null) {
+          reject(err);
+        }
+        else {
+          resolve(translation.translatedText);
+        }
+      });
+    });
+  }
 };
