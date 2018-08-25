9cbfc91c7124f0203bce8ac8ddc7905d76db6362
David Gu <david.gu@hotmail.com>
2018-08-25 08:45:00 -0400

    Fixed function call

--- stdlib/davidgu/polycode/functions/__main__.js
+++ stdlib/davidgu/polycode/functions/__main__.js
@@ -7,9 +7,10 @@ const lib = require("lib");
 * @returns {any}
 */
 module.exports = (doc="", config, map, context, callback) => {
+  console.log(context.service.identifier)
   lib[`${context.service.identifier}.get_tokens`](doc, (err, tokens) => {
     for (var token in tokens) token.translated = "ttt";
-    lib[`${context.service.identifier}.update_docs`](doc, tokens, (err, doc) => {
+    lib[`${context.service.identifier}.update_doc`](doc, tokens, (err, doc) => {
       callback(null, doc);
     });
   });
