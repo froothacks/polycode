673e3e409eedd4b2b05cfa592c00d555dda2b291
Ethan Guo <ethguo@gmail.com>
2018-08-25 09:53:36 -0400

    Removed console.logs, added call to token_mapper in main

--- stdlib/davidgu/polycode/functions/__main__.js
+++ stdlib/davidgu/polycode/functions/__main__.js
@@ -9,13 +9,10 @@ const lib = require("lib");
 module.exports = (doc="", config, map, context, callback) => {
   console.log(context.service.identifier)
   lib[`${context.service.identifier}.get_tokens`](doc, (err, tokens) => {
-    console.log(tokens)
-    for (var i = 0; i < tokens.length; i++) {
-      tokens[i].translated = ":)";
-    }
-    console.log(tokens)
-    lib[`${context.service.identifier}.update_doc`](doc, tokens, (err, doc) => {
-      callback(null, doc);
+    lib[`${context.service.identifier}.token_mapper`](tokens, (err, tokens) => {
+      lib[`${context.service.identifier}.update_doc`](doc, tokens, (err, doc) => {
+        callback(null, doc);
+      });
     });
   });
 };
--- stdlib/davidgu/polycode/functions/get_tokens.js
+++ stdlib/davidgu/polycode/functions/get_tokens.js
@@ -10,16 +10,13 @@ module.exports = (doc, callback) => {
   var nextToken = filbert.tokenize(doc);
 
   var t = nextToken();
-  console.log(t)
   while (t["type"]["type"] !== "eof") {
     if (t["type"]["type"] === "name") {
       tokens_.push(Object.assign({}, t));
     }
-    console.log(tokens_);
     t = nextToken();
   }
 
-  console.log("HIIII ", tokens_);
 
   callback(null, tokens_);
 };
