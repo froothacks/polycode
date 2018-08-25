c5df3a4b11ba86cd166f3dde9a209d6ec1d55966
Ethan Guo <ethguo@gmail.com>
2018-08-25 08:41:50 -0400

    Context-sensitive maybe

--- stdlib/davidgu/polycode/functions/__main__.js
+++ stdlib/davidgu/polycode/functions/__main__.js
@@ -7,9 +7,9 @@ const lib = require("lib");
 * @returns {any}
 */
 module.exports = (doc="", config, map, context, callback) => {
-  lib.davidgu.polycode.get_tokens(doc, (err, tokens) => {
+  lib[`${context.service.identifier}.get_tokens`](doc, (err, tokens) => {
     for (var token in tokens) token.translated = "ttt";
-    lib.davidgu.polycode.update_docs(doc, tokens, (err, doc) => {
+    lib[`${context.service.identifier}.update_docs`](doc, tokens, (err, doc) => {
       callback(null, doc);
     });
   });
