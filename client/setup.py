c237c6223a5865138240fbe6f5a976b8fd65be74
David Gu <david.gu@hotmail.com>
2018-08-26 07:10:27 -0400

    Updated version

--- client/setup.py
+++ client/setup.py
@@ -12,7 +12,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='polycode',
-    version='1.0.2',
+    version='1.0.4',
     description='Translate your code to a different spoken language',
     long_description=long_description,
     long_description_content_type='text/markdown',
