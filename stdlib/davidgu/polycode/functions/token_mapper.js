fcc11bb77a7b11071f6abdebc657be95919fe8c7
Advait-M <advait.maybhate9@gmail.com>
2018-08-25 23:24:24 -0400

    Fixes mkdir errors on stdlib deploy
    Fixes calls to Google Translate API

--- stdlib/davidgu/polycode/functions/token_mapper.js
+++ stdlib/davidgu/polycode/functions/token_mapper.js
@@ -1,6 +1,376 @@
-const translate = require('google-translate-api');
-const lib = require('lib');
 const Case = require('case');
+
+var querystring = require('querystring');
+
+var got = require('got');
+var safeEval = require('safe-eval');
+
+/**
+ * Last update: 2016/06/26
+ * https://translate.google.com/translate/releases/twsfe_w_20160620_RC00/r/js/desktop_module_main.js
+ *
+ * Everything between 'BEGIN' and 'END' was copied from the url above.
+ */
+
+function sM(a) {
+    var b;
+    if (null !== yr)
+        b = yr;
+    else {
+        b = wr(String.fromCharCode(84));
+        var c = wr(String.fromCharCode(75));
+        b = [b(), b()];
+        b[1] = c();
+        b = (yr = window[b.join(c())] || "") || ""
+    }
+    var d = wr(String.fromCharCode(116))
+        , c = wr(String.fromCharCode(107))
+        , d = [d(), d()];
+    d[1] = c();
+    c = "&" + d.join("") + "=";
+    d = b.split(".");
+    b = Number(d[0]) || 0;
+    for (var e = [], f = 0, g = 0; g < a.length; g++) {
+        var l = a.charCodeAt(g);
+        128 > l ? e[f++] = l : (2048 > l ? e[f++] = l >> 6 | 192 : (55296 == (l & 64512) && g + 1 < a.length && 56320 == (a.charCodeAt(g + 1) & 64512) ? (l = 65536 + ((l & 1023) << 10) + (a.charCodeAt(++g) & 1023),
+            e[f++] = l >> 18 | 240,
+            e[f++] = l >> 12 & 63 | 128) : e[f++] = l >> 12 | 224,
+            e[f++] = l >> 6 & 63 | 128),
+            e[f++] = l & 63 | 128)
+    }
+    a = b;
+    for (f = 0; f < e.length; f++)
+        a += e[f],
+            a = xr(a, "+-a^+6");
+    a = xr(a, "+-3^+b+-f");
+    a ^= Number(d[1]) || 0;
+    0 > a && (a = (a & 2147483647) + 2147483648);
+    a %= 1E6;
+    return c + (a.toString() + "." + (a ^ b))
+}
+
+var yr = null;
+var wr = function(a) {
+    return function() {
+        return a
+    }
+}
+    , xr = function(a, b) {
+    for (var c = 0; c < b.length - 2; c += 3) {
+        var d = b.charAt(c + 2)
+            , d = "a" <= d ? d.charCodeAt(0) - 87 : Number(d)
+            , d = "+" == b.charAt(c + 1) ? a >>> d : a << d;
+        a = "+" == b.charAt(c) ? a + d & 4294967295 : a ^ d
+    }
+    return a
+};
+
+var window = {
+    TKK: '0'
+};
+
+function updateTKK() {
+    return new Promise(function (resolve, reject) {
+        var now = Math.floor(Date.now() / 3600000);
+
+        if (Number(window.TKK.split('.')[0]) === now) {
+            resolve();
+        } else {
+            got('https://translate.google.com').then(function (res) {
+                var code = res.body.match(/TKK=(.*?)\(\)\)'\);/g);
+
+                if (code) {
+                    eval(code[0]);
+                    /* eslint-disable no-undef */
+                    if (typeof TKK !== 'undefined') {
+                        window.TKK = TKK;
+                        // config.set('TKK', TKK);
+                    }
+                    /* eslint-enable no-undef */
+                }
+
+                /**
+                 * Note: If the regex or the eval fail, there is no need to worry. The server will accept
+                 * relatively old seeds.
+                 */
+
+                resolve();
+            }).catch(function (err) {
+                var e = new Error();
+                e.code = 'BAD_NETWORK';
+                e.message = err.message;
+                reject(e);
+            });
+        }
+    });
+}
+
+function get(text) {
+    return updateTKK().then(function () {
+        var tk = sM(text);
+        tk = tk.replace('&tk=', '');
+        return {name: 'tk', value: tk};
+    }).catch(function (err) {
+        throw err;
+    });
+}
+
+/**
+ *
+ * Generated from https://translate.google.com
+ *
+ * The languages that Google Translate supports (as of 5/15/16) alongside with their ISO 639-1 codes
+ * See https://en.wikipedia.org/wiki/List_of_ISO_639-1_codes
+ */
+var langs = {
+    'auto': 'Automatic',
+    'af': 'Afrikaans',
+    'sq': 'Albanian',
+    'am': 'Amharic',
+    'ar': 'Arabic',
+    'hy': 'Armenian',
+    'az': 'Azerbaijani',
+    'eu': 'Basque',
+    'be': 'Belarusian',
+    'bn': 'Bengali',
+    'bs': 'Bosnian',
+    'bg': 'Bulgarian',
+    'ca': 'Catalan',
+    'ceb': 'Cebuano',
+    'ny': 'Chichewa',
+    'zh-cn': 'Chinese Simplified',
+    'zh-tw': 'Chinese Traditional',
+    'co': 'Corsican',
+    'hr': 'Croatian',
+    'cs': 'Czech',
+    'da': 'Danish',
+    'nl': 'Dutch',
+    'en': 'English',
+    'eo': 'Esperanto',
+    'et': 'Estonian',
+    'tl': 'Filipino',
+    'fi': 'Finnish',
+    'fr': 'French',
+    'fy': 'Frisian',
+    'gl': 'Galician',
+    'ka': 'Georgian',
+    'de': 'German',
+    'el': 'Greek',
+    'gu': 'Gujarati',
+    'ht': 'Haitian Creole',
+    'ha': 'Hausa',
+    'haw': 'Hawaiian',
+    'iw': 'Hebrew',
+    'hi': 'Hindi',
+    'hmn': 'Hmong',
+    'hu': 'Hungarian',
+    'is': 'Icelandic',
+    'ig': 'Igbo',
+    'id': 'Indonesian',
+    'ga': 'Irish',
+    'it': 'Italian',
+    'ja': 'Japanese',
+    'jw': 'Javanese',
+    'kn': 'Kannada',
+    'kk': 'Kazakh',
+    'km': 'Khmer',
+    'ko': 'Korean',
+    'ku': 'Kurdish (Kurmanji)',
+    'ky': 'Kyrgyz',
+    'lo': 'Lao',
+    'la': 'Latin',
+    'lv': 'Latvian',
+    'lt': 'Lithuanian',
+    'lb': 'Luxembourgish',
+    'mk': 'Macedonian',
+    'mg': 'Malagasy',
+    'ms': 'Malay',
+    'ml': 'Malayalam',
+    'mt': 'Maltese',
+    'mi': 'Maori',
+    'mr': 'Marathi',
+    'mn': 'Mongolian',
+    'my': 'Myanmar (Burmese)',
+    'ne': 'Nepali',
+    'no': 'Norwegian',
+    'ps': 'Pashto',
+    'fa': 'Persian',
+    'pl': 'Polish',
+    'pt': 'Portuguese',
+    'ma': 'Punjabi',
+    'ro': 'Romanian',
+    'ru': 'Russian',
+    'sm': 'Samoan',
+    'gd': 'Scots Gaelic',
+    'sr': 'Serbian',
+    'st': 'Sesotho',
+    'sn': 'Shona',
+    'sd': 'Sindhi',
+    'si': 'Sinhala',
+    'sk': 'Slovak',
+    'sl': 'Slovenian',
+    'so': 'Somali',
+    'es': 'Spanish',
+    'su': 'Sundanese',
+    'sw': 'Swahili',
+    'sv': 'Swedish',
+    'tg': 'Tajik',
+    'ta': 'Tamil',
+    'te': 'Telugu',
+    'th': 'Thai',
+    'tr': 'Turkish',
+    'uk': 'Ukrainian',
+    'ur': 'Urdu',
+    'uz': 'Uzbek',
+    'vi': 'Vietnamese',
+    'cy': 'Welsh',
+    'xh': 'Xhosa',
+    'yi': 'Yiddish',
+    'yo': 'Yoruba',
+    'zu': 'Zulu'
+};
+
+function translate(text, opts) {
+    opts = opts || {};
+
+    var e;
+    [opts.from, opts.to].forEach(function (lang) {
+        if (lang && !isSupported(lang)) {
+            e = new Error();
+            e.code = 400;
+            e.message = 'The language \'' + lang + '\' is not supported';
+        }
+    });
+    if (e) {
+        return new Promise(function (resolve, reject) {
+            reject(e);
+        });
+    }
+
+    opts.from = opts.from || 'auto';
+    opts.to = opts.to || 'en';
+
+    opts.from = getCode(opts.from);
+    opts.to = getCode(opts.to);
+
+    return get(text).then(function (token) {
+        var url = 'https://translate.google.com/translate_a/single';
+        var data = {
+            client: 't',
+            sl: opts.from,
+            tl: opts.to,
+            hl: opts.to,
+            dt: ['at', 'bd', 'ex', 'ld', 'md', 'qca', 'rw', 'rm', 'ss', 't'],
+            ie: 'UTF-8',
+            oe: 'UTF-8',
+            otf: 1,
+            ssel: 0,
+            tsel: 0,
+            kc: 7,
+            q: text
+        };
+        data[token.name] = token.value;
+
+        return url + '?' + querystring.stringify(data);
+    }).then(function (url) {
+        return got(url).then(function (res) {
+            var result = {
+                text: '',
+                from: {
+                    language: {
+                        didYouMean: false,
+                        iso: ''
+                    },
+                    text: {
+                        autoCorrected: false,
+                        value: '',
+                        didYouMean: false
+                    }
+                },
+                raw: ''
+            };
+
+            if (opts.raw) {
+                result.raw = res.body;
+            }
+
+            var body = safeEval(res.body);
+            body[0].forEach(function (obj) {
+                if (obj[0]) {
+                    result.text += obj[0];
+                }
+            });
+
+            if (body[2] === body[8][0][0]) {
+                result.from.language.iso = body[2];
+            } else {
+                result.from.language.didYouMean = true;
+                result.from.language.iso = body[8][0][0];
+            }
+
+            if (body[7] && body[7][0]) {
+                var str = body[7][0];
+
+                str = str.replace(/<b><i>/g, '[');
+                str = str.replace(/<\/i><\/b>/g, ']');
+
+                result.from.text.value = str;
+
+                if (body[7][5] === true) {
+                    result.from.text.autoCorrected = true;
+                } else {
+                    result.from.text.didYouMean = true;
+                }
+            }
+
+            return result;
+        }).catch(function (err) {
+            var e;
+            e = new Error();
+            if (err.statusCode !== undefined && err.statusCode !== 200) {
+                e.code = 'BAD_REQUEST';
+            } else {
+                e.code = 'BAD_NETWORK';
+            }
+            throw e;
+        });
+    });
+}
+
+/**
+ * Returns the ISO 639-1 code of the desiredLang – if it is supported by Google Translate
+ * @param {string} desiredLang – the name or the code of the desired language
+ * @returns {string|boolean} The ISO 639-1 code of the language or false if the language is not supported
+ */
+function getCode(desiredLang) {
+    if (!desiredLang) {
+        return false;
+    }
+    desiredLang = desiredLang.toLowerCase();
+
+    if (langs[desiredLang]) {
+        return desiredLang;
+    }
+
+    var keys = Object.keys(langs).filter(function (key) {
+        if (typeof langs[key] !== 'string') {
+            return false;
+        }
+
+        return langs[key].toLowerCase() === desiredLang;
+    });
+
+    return keys[0] || false;
+}
+
+/**
+ * Returns true if the desiredLang is supported by Google Translate and false otherwise
+ * @param desiredLang – the ISO 639-1 code or the name of the desired language
+ * @returns {boolean}
+ */
+function isSupported(desiredLang) {
+    return Boolean(getCode(desiredLang));
+}
 
 /**
 * @param {array} tokens
