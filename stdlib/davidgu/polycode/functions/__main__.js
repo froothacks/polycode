d55026c1329ca9a05e69be4b22b91f9a84bc751b
Ethan Guo <ethguo@gmail.com>
2018-08-26 07:31:34 -0400

    Javascript!

--- stdlib/davidgu/polycode/functions/__main__.js
+++ stdlib/davidgu/polycode/functions/__main__.js
@@ -1,16 +1,17 @@
 const lib = require("lib");
 
 /**
-* @param {string} doc Plaintext of the document to be translated
-* @param {string} from ISO 639-1 language code for 
-* @param {string} to
-* @param {string} map
-* @param {string} scriptlang
-* @returns {any} 
+* @param {string} doc   Plaintext of the document to be translated
+* @param {string} from  2-letter code of language to translate from
+* @param {string} to    2-letter code of language to be translate into
+* @param {string} map   JSON-stringified string of map file
+* @param {string} ext   File extension of doc
+* @returns {object} 
 */
-module.exports = async (doc, from="EN", to="FR", map="{}", scriptlang="python", context) => {
+module.exports = async (doc, from="EN", to="FR", map="{}", ext=".py", context) => {
   map = JSON.parse(map);
-  var tokens = await lib[`${context.service.identifier}.get_tokens`](doc, scriptlang);
+  var lang = langForExt[ext];
+  var tokens = await lib[`${context.service.identifier}.get_tokens`](doc, lang);
   var result = await lib[`${context.service.identifier}.token_mapper`](tokens, from, to, map);
   tokens = result["tokens"];
   map = result["map"];
@@ -18,3 +19,9 @@ module.exports = async (doc, from="EN", to="FR", map="{}", scriptlang="python", 
 
   return {"doc": doc, "map": map}
 };
+
+langForExt = {
+  ".py": "python",
+  ".js": "javascript",
+  ".jsx": "javascript"
+}
